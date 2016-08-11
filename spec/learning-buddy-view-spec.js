ffef278438a99661eaaff757fc14a5a862800160
Collin Mutembei <mutembei.collin@gmail.com>
2016-08-11 14:43:21 +0300

    Add test for learning-buddy-view

---
+++ spec/learning-buddy-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import CollinWordCountView from '../lib/learning-buddy-view';
+
+describe('LearningBuddyView', () => {
+  it('has one valid test', () => {
+    // expect('life').toBe('easy');
+  });
+});
